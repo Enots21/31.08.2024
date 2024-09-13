1ea12fe242ad4d9979d2d68f849784d697ab097f
Енот <68638093+Enots21@users.noreply.github.com>
2024-09-13 17:06:23 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,13 @@
+import random
+
+def draw_are(number): # функция
+    graw_ = '' #Пустой список
+    for i in range(1, number): #Идёт счёт от 1
+        for j in range(i+1, number): #Идёт счёт от 1 + 1 и веденное число
+            if number % (i+j) == 0:
+                graw_ += str(i) + str(j) #Перевод + сложение
+    return graw_
+
+
+print('Введите число')
+print(draw_are(int(input())))
